d8907d9012123caffb65fcf8703008e3351847ea
William Lima <107582520+wcclima@users.noreply.github.com>
2025-01-30 15:06:52 +0100

    Update streamlit_app.py
    Update to present the API response in the forma of a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + "!", icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
