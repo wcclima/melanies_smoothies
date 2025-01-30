ea35a385a46fd28900f4446672cce995de9bf519
William Lima <107582520+wcclima@users.noreply.github.com>
2025-01-30 16:59:37 +0100

    Update streamlit_app.py
    Testing the SEARCH_ON column

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,11 @@ session = cnx.session()
 my_dataframe = session.table(
     "smoothies.public.fruit_options"
 ).select(
-    col("fruit_name")
+    col("fruit_name"),
+    col("search_on")
 )
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
