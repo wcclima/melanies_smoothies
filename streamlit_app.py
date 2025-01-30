6c920bfc368ca3b8f81c0e5a89df5621c8df37a7
William Lima <107582520+wcclima@users.noreply.github.com>
2025-01-30 15:09:19 +0100

    Update streamlit_app.py
    Remove commented lines and put the API request and dataframe presentation inside the FOR loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,25 +34,16 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
-    ingredients_string = ingredients_string.rstrip()
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    # ingredients_string = ingredients_string.rstrip()
-    # st.write(ingredients_string)
+    ingredients_string = ingredients_string.rstrip()
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-
-    # st.write(my_insert_stmt)
-    # st.stop()
 
     time_to_insert = st.button("Submit Order")
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + "!", icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
