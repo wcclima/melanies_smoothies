cd87c3a85550a98a09e7a2fcc8fa45cb1e760401
William Lima <107582520+wcclima@users.noreply.github.com>
2025-01-30 15:26:58 +0100

    Update streamlit_app.py
    - Add a header for the presentationof the API response in the dataframe format
    
    - Make the API to request information about the current fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     ingredients_string = ingredients_string.rstrip()
