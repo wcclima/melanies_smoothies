517429cabb1c99c2aeb28adf8107732854273661
William Lima <107582520+wcclima@users.noreply.github.com>
2025-01-30 17:09:28 +0100

    Update streamlit_app.py
    Correct typo in the API request address

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
         st.write('The search value for ' + fruit + ' is ' + search_on + '.')
         
         st.subheader(fruit + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on )
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     ingredients_string = ingredients_string.rstrip()
